48815316b78f972455733683cb919b737ce346a1
Satya Sirigudi <sirigudi@yahoo.com>
2016-10-30 07:02:26 -0700

    Add commons-lang3 dependency

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,6 +2,7 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
@@ -17,11 +18,17 @@ public class Application {
 			System.out.println("Greeting: " + greeting);
 		}
 	}
+	
+	public int countWords(String words) {
+		String[] separateWords = StringUtils.split(words, ' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		System.out.println("Word count: " + app.countWords("I have four words"));
     }
 }
